f9d35d92d9dfff695cb177a7df4bdf9b70e35130
Tamique1 <morristamique@gmail.com>
2026-03-04 11:39:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
+const toggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
 toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
-});
-
-const form = document.getElementById("contact-form");
-const formMessage = document.getElementById("form-message");
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    if (name === "" || email === "" || message === "") {
-        formMessage.style.color = "red";
-        formMessage.textContent = "Please fill in all fields.";
-    } else {
-        formMessage.style.color = "green";
-        formMessage.textContent = "Message sent successfully!";
-        form.reset();
-    }
 });
